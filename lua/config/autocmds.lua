a7942b32b5bdabebebbbd8695de0be873150ae26
mcbunkus <austenlebeau@gmail.com>
2026-03-01 11:50:44 -0600

    theme and configs

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,5 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- local ft_options_group = vim.api.nvim_create_augroup("FileTypeOptionsGroup", { clear = true })
